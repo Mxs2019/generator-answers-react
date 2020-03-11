1a638205f18f477ff7e6fcfe3658d46264e81223
Max Shaw <maxdshaw@gmail.com>
2020-03-11 10:57:39 -0400

    remove react import

--- generators/addConfig/templates/verticalConfig.tsx
+++ generators/addConfig/templates/verticalConfig.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import * as T from "../../answers/Types";
 
 const <%= verticalKey %>Config: T.VerticalConfig = {
   title: "<%= verticalKey %>",
   card: {
-    mapping: {
-    }
+    mapping: {}
   }
 };
 
