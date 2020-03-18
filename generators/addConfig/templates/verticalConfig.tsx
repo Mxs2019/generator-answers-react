75f830d4945a434a2119376ffe41d6ab1fc45dd8
Max Shaw <maxdshaw@gmail.com>
2020-03-18 09:27:35 -0400

    update to vertical config

--- generators/addConfig/templates/verticalConfig.tsx
+++ generators/addConfig/templates/verticalConfig.tsx
@@ -1,12 +1,18 @@
+import React from "react"
 import * as T from "../../answers/Types";
 import * as Icons from "react-icons/fa"
+import StandardCard from "../../answers/overridableComponents/resultCards/cardContainers/StandardCard"
+
+const <%= verticalKey %>Card = ({ profile, ...props }) => (
+  <StandardCard title={profile.name} url={profile.website} {...props}>
+    {profile.description}
+  </StandardCard>
+)
 
 const <%= verticalKey %>Config: T.VerticalConfig = {
   title: "<%= verticalKey %>",
   icon: Icons.FaCircle,
-  card: {
-    mapping: {}
-  }
+  card: <%= verticalKey %>Card
 };
 
 export default <%= verticalKey %>Config;
